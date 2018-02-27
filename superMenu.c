20580e9689d80b598669ec89690236fad103a148
Nathan Beach <nathanbeach@mail.weber.edu>
2018-02-27 11:51:23 -0700

    Initial commit

---
+++ superMenu.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  superMenu.c
+ *
+ *    Description:  My super menu in C
+ *
+ *        Version:  1.0
+ *        Created:  02/27/2018 11:44:42 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Nathan Beach (), nathanbeach@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    printf("Hello super program\n");
+    return 0;
+}
+// Function Definitions
+
+
